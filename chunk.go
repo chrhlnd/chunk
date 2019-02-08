e2ea951e9482ed68998e16fa9513f83b4fe8faee
Chris Holland <cholland@littleorbit.com>
2019-02-08 13:29:10 -0800

    adding a way to specify group size

--- chunk.go
+++ chunk.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sort"
 )
 
 // InspectConfig - configure parameters into the inspection process, passing nil will use default
@@ -66,6 +67,10 @@ var defaultConfig = InspectConfig{
 	"md5",
 }
 
+func GetDefaultConfig() InspectConfig {
+	return defaultConfig
+}
+
 type usage struct {
 	pat    uint32
 	cnt    int
@@ -93,6 +98,37 @@ func (fcd *FileChunkDesc) Write(out io.Writer) error {
 // ReadFileChunkDesc - create a FileChunkDesc from a serialized version
 func ReadFileChunkDesc(in bufio.Reader) (*FileChunkDesc, error) {
 	return readPatchInfo(in)
+}
+
+// Analyze2 - inspect the input stream for a pattern then write out the chunks to the out stream
+func Analyze2(in io.ReadSeeker, config *InspectConfig) (*FileChunkDesc, *Stats, error) {
+
+	if config == nil {
+		config = &defaultConfig
+	}
+
+	stats := &Stats{}
+	stats.AnalyzeSize = config.ScanSize
+
+	pats := collectPatterns2(in, config, stats)
+	pick := pickPattern2(pats, stats)
+
+	stats.PatternUsed = pick.pat
+
+	chunks, err := groupChunks(in, pick.pat, config.ChunkSize, pick.cnt, config.HashMeth, stats)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ret := FileChunkDesc{
+		HashMeth:  config.HashMeth,
+		Pat:       pick.pat,
+		ChunkSize: config.ChunkSize,
+		Chunks:    chunks,
+		Count:     len(chunks),
+	}
+
+	return &ret, stats, nil
 }
 
 // Analyze - inspect the input stream for a pattern then write out the chunks to the out stream
@@ -511,6 +547,87 @@ func debugDump(fname string, chunks []chunk) {
 	f.Close()
 }
 
+func collectPatterns2(in io.Reader, config *InspectConfig, stats *Stats) ([]*usage) {
+	var off int64
+
+	pats := make([]*usage, 0, 1024 * 10)
+
+	path := make(map[uint32]int)
+
+	var data [4]byte
+
+	var pat uint32
+
+	for sz, err := in.Read(data[:]); err == nil || (err == io.EOF && sz > 0); sz, err = in.Read(data[:]) {
+		pat = 0
+		for i := 0; i < sz; i++ {
+			pat |= uint32(data[i]) << uint(byteBitSize*((patternSize-1)-i))
+		}
+
+		var st *usage
+
+		idx, ok := path[pat]
+		if ok {
+			st = pats[idx]
+			st.cnt++
+
+			if idx > 0 && pats[idx-1].cnt < st.cnt {
+				find := sort.Search(len(pats)-idx, func (i int) bool { return pats[i].cnt <= st.cnt; })
+				if find < len(pats)-idx {
+				//	log.Print("Find+1 ", find+1, " find ", find, " index ", idx)
+					copy(pats[find+1:], pats[find:idx])
+					pats[find] = st
+					path[pat] = find
+				}
+			}
+
+			//find := idx - 1
+			//for ;find > -1 && st.cnt > pats[find].cnt; find-- { }
+			//find++
+			//copy(pats[find+1:], pats[find:idx])
+			//pats[find] = st
+			//path[pat] = find
+		} else {
+			st = &usage{pat: pat, min: off, cnt: 1}
+			if len(pats) < cap(pats) || off < config.ScanSize || config.ScanSize == -1 {
+				pats = append(pats,st)
+				path[pat] = len(pats)-1
+			} else {
+				// else don't add it
+				off += int64(sz)
+				if err == io.EOF {
+					break
+				} else {
+					continue
+				}
+			}
+		}
+
+		if st.max < off {
+			if st.max == off-patternSize {
+				st.conseq++
+				st.cnt--
+			} else {
+				if st.max != 0 {
+					st.avg = (st.avg + (off - st.max)) / 2
+				}
+			}
+			st.max = off
+		}
+
+		off += int64(sz)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	log.Print("Done with analasis")
+
+	stats.AnalyzeSize = off
+
+	return pats
+}
+
 func collectPatterns(in io.Reader, config *InspectConfig, stats *Stats) map[uint32]usage {
 	var off int64
 
@@ -520,7 +637,7 @@ func collectPatterns(in io.Reader, config *InspectConfig, stats *Stats) map[uint
 
 	var pat uint32
 
-	for sz, err := in.Read(data[:]); (err == nil || (err == io.EOF && sz > 0)) && (off == -1 || off < config.ScanSize); sz, err = in.Read(data[:]) {
+	for sz, err := in.Read(data[:]); (err == nil || (err == io.EOF && sz > 0)) && (config.ScanSize == -1 || off < config.ScanSize); sz, err = in.Read(data[:]) {
 		pat = 0
 		for i := 0; i < sz; i++ {
 			pat |= uint32(data[i]) << uint(byteBitSize*((patternSize-1)-i))
@@ -553,6 +670,39 @@ func collectPatterns(in io.Reader, config *InspectConfig, stats *Stats) map[uint
 	stats.AnalyzeSize = off
 
 	return pats
+}
+
+func pickPattern2(pats []*usage, stats *Stats) usage {
+	var pick *usage
+
+	for _, v := range pats[0:10] {
+		if v.pat == 0 {
+			if len(pats) == 1 {
+				pick = v
+			}
+			continue
+		} else {
+			if pick == nil || pick.cnt == 0 {
+				pick = v
+			}
+			if v.cnt <= int(float32(pick.cnt)*.4) {
+				pick = v
+				break
+			}
+		}
+	}
+
+	for _, v := range pats[0:10] {
+		stats.Top10Patterns = append(stats.Top10Patterns, PatternInfo{Count: v.cnt,
+			AvgByteSpan: v.avg,
+			Conseq:      v.conseq,
+			Pat:         v.pat,
+			PosMax:      v.max,
+			PosMin:      v.min,
+		})
+	}
+
+	return *pick
 }
 
 func pickPattern(pats map[uint32]usage, stats *Stats) usage {
--- chunkcmd/main.go
+++ chunkcmd/main.go
@@ -15,6 +15,9 @@ var chk = flag.Bool("chk", false, "check patch info")
 var pinfo = flag.String("pinfo", ".pinfo", "Use this extension when scanning for patch files on the commandline")
 var cpuprof = flag.Bool("cpuprofile", false, "write cpu profile")
 var huse = flag.String("hash", "md5", "md5|sha256 sha is slower may matter")
+var ssize = flag.Int("ssize", 10*1024*1024, "set the scan size to find a pattern")
+var gsize = flag.Int("gsize", 400*1024, "set the grouping size to find a pattern")
+var meth = flag.String("meth", "one", "pick one or two")
 
 const bufferSize = 1024 * 64
 
@@ -58,16 +61,33 @@ func main() {
 
 				defer fout.Close()
 
-				fileDesc, stats, err := chunk.Analyze(chunk.NewReadSeeker(f, bufferSize), nil)
-				if err != nil {
-					log.Fatal(err)
-				}
+				cfg := chunk.GetDefaultConfig()
+				cfg.ScanSize = int64(*ssize)
+				cfg.ChunkSize = int64(*gsize)
+				if *meth == "one" {
+					fileDesc, stats, err := chunk.Analyze(chunk.NewReadSeeker(f, bufferSize), &cfg)
+					if err != nil {
+						log.Fatal(err)
+					}
 
-				printStats(f.Name(), stats)
+					printStats(f.Name(), stats)
 
-				err = fileDesc.Write(fout)
-				if err != nil {
-					log.Fatal(err)
+					err = fileDesc.Write(fout)
+					if err != nil {
+						log.Fatal(err)
+					}
+				} else {
+					fileDesc, stats, err := chunk.Analyze2(chunk.NewReadSeeker(f, bufferSize), &cfg)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					printStats(f.Name(), stats)
+
+					err = fileDesc.Write(fout)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
 			}
 
