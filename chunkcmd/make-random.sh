753e03d5992a224c784cf90e199e9cc7a8dbbbb2
cholland <chris.holland@littleorbit.com>
2019-02-07 21:08:46 -0800

    add something for posterity

---
+++ chunkcmd/make-random.sh
@@ -0,0 +1,8 @@
+head -c 5M </dev/urandom > randomfile
+echo 'hihihihihihihihihihihihihihihihihihihi' >> randomfile
+head -c 5M </dev/urandom >> randomfile
+echo 'hihihihihihihihihihihihihihihihihihihi' >> randomfile
+head -c 5M </dev/urandom >> randomfile
+echo 'hihihihihihihihihihihihihihihihihihihi' >> randomfile
+head -c 5M </dev/urandom >> randomfile
+echo 'hihihihihihihihihihihihihihihihihihihi' >> randomfile
